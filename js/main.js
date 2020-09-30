a7cb5c6b1356528cf304577b06232b066e2a5d6a
bomingzheng <bomingzheng@126.com>
2020-09-30 14:02:30 +0800

    test09

---
+++ js/main.js
@@ -0,0 +1,27 @@
+alert("hello python! ");
+//js单行注释  /*多行注释*/
+// 变量，需要声明
+// 变量命名规则使用短名称见名知意，必须以字母开头，可以用￥和_开头但是不推荐，由字母和数字组成遵循驼峰原则，大小写敏感
+
+var name = '月下无限连';          //先声明在赋值
+var age;                        //只声明不赋值
+alert(name);                    // 弹窗显示
+alert(age);                     //只声明不赋值会返回一个undefined
+age=18;                          //声明后可以直接赋值
+dent =20;                         // 不声明，直接赋值他不报错会变成windows的一个属性
+alert(window.dent)
+console.log(name);              //输出到控制台
+console.log(age)
+var a =10, b=15, c=25;           // 多个变量赋值
+
+// 数据类型： 数字，字符串，Allay，null，undefined boolean
+// js是一门弱类型的语言
+var a=12;    //数字类型
+var b=1.3;  //数字类型
+var t='asw';   // 字符串
+var list=Array(1,2,3,4,5);   //Allay:相当于python的列表可通过下标取值
+// Allay属性
+
+
+
+
